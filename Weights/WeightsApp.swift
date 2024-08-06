a246909328888ba0db7c981183f3c2f101f4565f
Matt Linder <linder2015@outlook.com>
2024-08-05 20:11:52 -0400

    Initial Commit

---
+++ Weights/WeightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightsApp.swift
+//  Weights
+//
+//  Created by Matt Linder on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WeightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
